7e80535eb38510fbf956c45e34c03bcb8f655199
Neerhom <32304258+Neerhom@users.noreply.github.com>
2018-03-16 16:09:31 +0200

    Add files via upload

---
+++ COM3D2.Creator_SaveFix/COM3D2.Creator_SaveFix.Hook/COM3D2.Creator_SaveFix.Hook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COM3D2.Creator_SaveFix.Hook
+{
+    public static class savefix
+    {
+        public static void GetPropStringFix(ref Dictionary<string, MaidProp> m_dicMaidProp, ref string tag)
+        {
+            if (!m_dicMaidProp.ContainsKey(tag))
+            {
+                tag = "null_mpn";
+            }
+        }
+
+
+        public static void CM_dic_fix()
+        {
+         CM3.dicDelItem[MPN.null_mpn] = string.Empty;
+        
+            }
+
+        public static void MaidPropDesFix( ref string name)
+        {
+            int idx = 0;
+
+            try
+            {
+                idx = (int)Enum.Parse(typeof(MPN), name, false);
+
+            }
+            catch (Exception e)
+            {
+                name = "null_mpn";
+
+            }
+
+        }
+    }
+
+   }
